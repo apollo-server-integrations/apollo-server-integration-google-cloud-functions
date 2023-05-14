fead6d8b8e09c2925efc3a5d39d24d9b345a0284
Ernesto Resende <dev@ernestoresende.com>
2023-05-14 15:01:54 -0300

    feat: added request & response proxies

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,97 @@
-import { ApolloServer, BaseContext, ContextFunction, HeaderMap } from '@apollo/server';
+import { ApolloServer, HeaderMap } from '@apollo/server';
 import { parse } from 'url';
-import { http } from '@google-cloud/functions-framework';
 
-import type { Request, Response, HttpFunction } from '@google-cloud/functions-framework';
-import type { WithRequired } from '@apollo/utils.withrequired';
+import { Request, Response } from '@google-cloud/functions-framework';
+// import type { WithRequired } from '@apollo/utils.withrequired';
 
-interface Options<Context extends BaseContext> {
-  context?: ContextFunction<Parameters<HttpFunction>, Context>;
-  functionTarget: string;
-}
+export function requestProxy(req: Request) {
+  const headers = new HeaderMap();
+  const requestMethod = req.method || 'POST';
+  const search = req.url ? parse(req.url).search || '' : '';
 
-type GoogleCloudApiHandler = (req: Request, res: Response) => Promise<unknown> | unknown;
-
-const defaultContext: ContextFunction<[], any> = async () => ({});
-
-/**
- * Creates a Google Cloud Functions handler for an `ApolloServer` instance.
- *
- * `startServerAndCreateGoogleCloudFunctionsHandler` requires a `functionTarget` string to be passed on the options object. The value of this string will be used to name the function entry point on Google Cloud Functions.
- *
- * @example
- *
- * startServerAndCreateGoogleCloudFunctionsHandler(server, {
- *   functionTarget: 'my-function-name',
- * });
- */
-export function startServerAndCreateGoogleCloudFunctionsHandler(
-  server: ApolloServer<BaseContext>,
-  options: Options<BaseContext>,
-): void;
-export function startServerAndCreateGoogleCloudFunctionsHandler<Context extends BaseContext>(
-  server: ApolloServer<Context>,
-  options: WithRequired<Options<Context>, 'context'>,
-): void;
-export function startServerAndCreateGoogleCloudFunctionsHandler<Context extends BaseContext>(
-  server: ApolloServer<Context>,
-  options: Options<Context>,
-) {
-  server.startInBackgroundHandlingStartupErrorsByLoggingAndFailingAllRequests();
-
-  const contextFunction = options?.context || defaultContext;
-
-  const handler: GoogleCloudApiHandler = async (req, res) => {
-    const headers = new HeaderMap();
-
-    for (const [key, value] of Object.entries(req.headers)) {
-      if (typeof value === 'string') {
-        headers.set(key, value);
-      }
+  for (const [key, value] of Object.entries(req.headers)) {
+    if (typeof value === 'string') {
+      headers.set(key, value);
     }
+  }
 
-    const httpGraphQLResponse = await server.executeHTTPGraphQLRequest({
-      context: () => contextFunction(req, res),
-      httpGraphQLRequest: {
-        body: req.body,
-        headers,
-        method: req.method || 'POST',
-        search: req.url ? parse(req.url).search || '' : '',
-      },
-    });
-
-    res.statusCode = httpGraphQLResponse.status || 200;
-
-    if (httpGraphQLResponse.body.kind === 'complete') {
-      res.send(httpGraphQLResponse.body.string);
-    } else {
-      for await (const chunk of httpGraphQLResponse.body.asyncIterator) {
-        res.write(chunk);
-      }
-      res.end();
-    }
+  return {
+    body: req.body,
+    headers,
+    method: requestMethod,
+    search,
   };
-
-  return http(options.functionTarget, handler);
 }
+
+type RequestProxyFunction = typeof requestProxy;
+export type RequestProxy = ReturnType<RequestProxyFunction>;
+
+export async function httpGraphQLResponse(server: ApolloServer, request: RequestProxy) {
+  const httpGraphQLResponse = await server.executeHTTPGraphQLRequest({
+    context: async () => ({}),
+    httpGraphQLRequest: request,
+  });
+
+  return httpGraphQLResponse;
+}
+
+type HttpGraphQLResponseFunction = typeof httpGraphQLResponse;
+export type HttpGraphQLResponse = Awaited<ReturnType<HttpGraphQLResponseFunction>>;
+
+export async function responseProxy(res: Response, httpGraphQLResponse: HttpGraphQLResponse) {
+  res.statusCode = httpGraphQLResponse.status || 200;
+
+  if (httpGraphQLResponse.body.kind === 'complete') {
+    res.send(httpGraphQLResponse.body.string);
+  } else {
+    for await (const chunk of httpGraphQLResponse.body.asyncIterator) {
+      res.write(chunk);
+    }
+    res.end();
+  }
+}
+
+// interface Options<Context extends BaseContext> {
+//   context?: ContextFunction<Parameters<HttpFunction>, Context>;
+// }
+
+// const defaultContext: ContextFunction<[], any> = async () => ({});
+
+// export function startServerAndCreateGoogleCloudFunctionsHandler(
+//   server: ApolloServer<BaseContext>,
+//   options: Options<BaseContext>,
+// ): void;
+// export function startServerAndCreateGoogleCloudFunctionsHandler<Context extends BaseContext>(
+//   server: ApolloServer<Context>,
+//   options: WithRequired<Options<Context>, 'context'>,
+// ): void;
+// export function startServerAndCreateGoogleCloudFunctionsHandler<Context extends BaseContext>(
+//   server: ApolloServer<Context>,
+//   options: Options<Context>,
+// ) {
+  // server.startInBackgroundHandlingStartupErrorsByLoggingAndFailingAllRequests();
+
+  // const contextFunction = options?.context || defaultContext;
+
+  // return async function (req: Request, res: Response) {
+  //   const httpGraphQLRequest = requestProxy(req);
+
+  //   const httpGraphQLResponse = await server.executeHTTPGraphQLRequest({
+  //     context: async () => contextFunction(req, res),
+  //     httpGraphQLRequest,
+  //   });
+
+  //   res.statusCode = httpGraphQLResponse.status || 200;
+
+  //   if (httpGraphQLResponse.body.kind === 'complete') {
+  //     res.send(httpGraphQLResponse.body.string);
+  //   } else {
+  //     for await (const chunk of httpGraphQLResponse.body.asyncIterator) {
+  //       res.write(chunk);
+  //     }
+  //     res.end();
+  //   }
+
+  //   return httpGraphQLResponse;
+  // };
+// }
